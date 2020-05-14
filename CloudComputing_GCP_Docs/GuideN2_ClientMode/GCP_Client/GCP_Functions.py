31fed410694842d3ada01f307eeb7cf687925521
Rubén D. González R <56765990+rdgonzalez14@users.noreply.github.com>
2020-05-14 01:01:53 -0300

    Add files via upload

---
+++ CloudComputing_GCP_Docs/GuideN2_ClientMode/GCP_Client/GCP_Functions.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Example of using the Compute Engine API to create and delete instances.
+Creates a new compute engine instance and uses it to apply a caption to
+an image.
+    https://cloud.google.com/compute/docs/tutorials/python-guide
+For more information, see the README.md under /compute.
+"""
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Importando librerías y/o funciones necesarias
+import time
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Definiendo funciones
+#-----------------------------------------------------------------------------------------------------------------------
+# [START list_instances]
+def list_instances(compute, project, zone):
+    result = compute.instances().list(project=project, zone=zone).execute()
+    return result['items'] if 'items' in result else None
+# [END list_instances]
+
+
+# [START create_instance]
+def create_instance(compute, project, zone, ncores, nram, name, bucket, start_script,image_name):
+    # Assign name of custom image to use for the VM creation
+    source_disk_image = r'https://www.googleapis.com/compute/v1/projects/'+project+r'/global/images/'+image_name
+
+    # Configure the machine
+    machine_type = "zones/%s/machineTypes/custom-%s-%s-ext" % (zone,ncores,nram)
+    startup_script = start_script
+
+    config = {
+        'name': name,
+        'machineType': machine_type,
+
+        # Specify the boot disk and the image to use as a source.
+        'disks': [
+            {
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage': source_disk_image,
+                }
+            }
+        ],
+
+        # Specify a network interface with NAT to access the public
+        # internet.
+        'networkInterfaces': [{
+            'network': 'global/networks/default',
+            'accessConfigs': [
+                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+            ]
+        }],
+
+        # Allow the instance to access cloud storage and logging.
+        'serviceAccounts': [{
+            'email': 'default',
+            'scopes': [
+                'https://www.googleapis.com/auth/devstorage.read_write',
+                'https://www.googleapis.com/auth/logging.write'
+            ]
+        }],
+
+        # Metadata is readable from the instance and allows you to
+        # pass configuration from deployment scripts to instances.
+        'metadata': {
+            'items': [{
+                # Startup script is automatically executed by the
+                # instance upon startup.
+                'key': 'startup-script-url',
+                'value': startup_script
+            }, {
+                'key': 'bucket',
+                'value': bucket
+            }]
+        }
+    }
+
+    return compute.instances().insert(
+        project=project,
+        zone=zone,
+        body=config).execute()
+# [END create_instance]
+
+
+# [START delete_instance]
+def delete_instance(compute, project, zone, name):
+    return compute.instances().delete(
+        project=project,
+        zone=zone,
+        instance=name).execute()
+# [END delete_instance]
+
+
+# [START wait_for_operation]
+def wait_for_operation(compute, project, zone, operation):
+    print('Waiting for operation to finish...')
+    while True:
+        result = compute.zoneOperations().get(
+            project=project,
+            zone=zone,
+            operation=operation).execute()
+
+        if result['status'] == 'DONE':
+            print("done.")
+            if 'error' in result:
+                raise Exception(result['error'])
+            return result
+
+        time.sleep(1)
+# [END wait_for_operation]
